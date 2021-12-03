8300a7455c34f2c8d7d0ff07e11cc9b636934c92
Jiri Zenzinger <juris.zen@seznam.cz>
2021-12-03 15:45:54 +0100

    Updating README.md and file structure

---
+++ README.md
@@ -0,0 +1,13 @@
+# Mars Weather Mobile Application
+Mars weather app is native iOS mobile application with 3 tabs. App is built with IonicFramework for school project **Programming Mobile Applications**.
+### Technologies
+* Ionic Framework
+* Vue.js
+* Typescript
+* Capacitor
+* Native iOS development
+***
+### API's
+* NASA Mars Weather API
+* NASA Rovers pictures API
+* Maybe will use Unsplash API later
--- src/views/Tab2.vue
+++ src/views/Tab2.vue
@@ -2,27 +2,37 @@
   <ion-page>
     <ion-header>
       <ion-toolbar>
-        <ion-title>Tab 2</ion-title>
+        <ion-title>Weather</ion-title>
       </ion-toolbar>
     </ion-header>
     <ion-content :fullscreen="true">
       <ion-header collapse="condense">
         <ion-toolbar>
-          <ion-title size="large">Tab 2</ion-title>
+          <ion-title size="large">Weather</ion-title>
         </ion-toolbar>
       </ion-header>
-      
-      <ExploreContainer name="Tab 2 page" />
+      <ion-grid class="ion-padding-lg">
+        <ion-row class="ion-align-items-center">
+          <ion-col>
+            <ion-button color="dark" v-on:click="openAlert()">Dark</ion-button>
+          </ion-col>
+        </ion-row>
+      </ion-grid>
     </ion-content>
   </ion-page>
 </template>
 
 <script lang="ts">
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
-import ExploreContainer from '@/components/ExploreContainer.vue';
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonGrid, IonRow, IonCol } from '@ionic/vue';
 
 export default  {
   name: 'Tab2',
-  components: { ExploreContainer, IonHeader, IonToolbar, IonTitle, IonContent, IonPage }
+  components: { IonHeader, IonToolbar, IonTitle, IonContent, IonPage, IonGrid, IonRow, IonCol },
+  methods: {
+    async openAlert() {
+      const message = "Testing something...";
+      alert(message);
+    }
+  }
 }
 </script>
