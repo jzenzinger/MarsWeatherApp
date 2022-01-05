179fa8f877385677018f59f87a1ae9b87f9843e2
jzenzinger <juris.zen@seznam.cz>
2022-01-05 20:28:38 +0100

    Tab1-Rovers API update and render function

--- src/views/Tab1.vue
+++ src/views/Tab1.vue
@@ -1,26 +1,51 @@
 <template>
   <ion-page>
-    <ion-header>
-      <ion-toolbar>
-        <ion-title>Tab 1</ion-title>
-      </ion-toolbar>
-    </ion-header>
     <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Tab 1</ion-title>
-        </ion-toolbar>
-      </ion-header>
-
+      <!-- Components template to render pictures in-->
+      <ion-card v-for="pic in photosArr" :key="pic.id" class="photos">
+        <ion-card-header class="ion-padding-horizontal">
+          <ion-card-title>{{ pic.camera.full_name }}</ion-card-title>
+          <ion-card-subtitle>Camera {{ pic.camera.name }}</ion-card-subtitle>
+          <ion-card-subtitle>Rover {{ pic.rover.name }}</ion-card-subtitle>
+        </ion-card-header>
+        <ion-card-content>
+          <ion-img :src="pic.img_src"></ion-img>
+        </ion-card-content>
+      </ion-card>
     </ion-content>
   </ion-page>
 </template>
 
 <script lang="ts">
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
 
-export default  {
+import { IonPage, IonContent, IonImg } from '@ionic/vue';
+import { defineComponent } from 'vue';
+
+export default defineComponent({
   name: 'Tab1',
-  components: { IonHeader, IonToolbar, IonTitle, IonContent, IonPage }
-}
+  components: { IonContent, IonPage, IonImg },
+  data() {
+    return {
+      photosArr: [],
+    };
+  },
+  methods: {
+    fetchRovers() {
+        const url = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=WpJlub0x6H6V2V5s4ryXm2j7LTES4HRhGrpAdPi2";
+        fetch(url)
+            .then(res => res.json())
+            .then(data => this.photosArr = data.photos)
+            .catch(err => console.log(err.message))
+    },
+  },
+  mounted() {
+    this.fetchRovers();
+  },
+});
 </script>
+
+<style scoped>
+.ion-page {
+  --ion-background-color: linear-gradient(0deg, rgba(54,10,2,1) 0%, rgba(143,47,32,1) 62%, rgba(207,55,10,1) 100%);
+}
+</style>
